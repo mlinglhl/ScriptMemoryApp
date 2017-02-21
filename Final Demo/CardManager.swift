366e9ec584629dbca6b55139ce0962f42b289240
mlinglhl <minhung.ling@gmail.com>
2017-02-21 12:45:38 -0500

    Updated to use managers

--- Final Demo/CardManager.swift
+++ Final Demo/CardManager.swift
@@ -9,8 +9,26 @@
 import UIKit
 
 class CardManager: NSObject {
-    var cardArray = [Card]()
-
+    var cardIndex = 0
+    var cardArray = [SampleCard.init(question: "Annabel: I have come all this way for the money Mr. Witherspoon. All six million dollars of it.", answer: "Harry: But I accepted the terms of my Uncle's will, and I'm here, you see, carrying out his wishes. So you people have lost."),
+                     SampleCard.init(question: "Annabel: Not yet we haven't. Not by a longshot. You see, there's a loophole.", answer: "Harry: Loophole? What loophole? Where?"),
+                     SampleCard.init(question: "Annabel: Well, when we received our copy of the will and tape, I noticed how detailed it was. All those social activities. All the things he wants to do and buy and wear... specific times you have to be specific places...", answer: "Harry: I'm doing the best I can!"),
+                     SampleCard.init(question: "Annabel: Yes, but make just one little slip--arrive somewhere one minute early or one minute late, put a pink flower in his buttonhole instead of red... you mess up one little detail, and accoridng to our lawyers, you'll be in default of the will!", answer: "Harry: What?!"),
+                     SampleCard.init(question: "Annabel: One little slip, and I take your Uncle, finish up the rest of his vacation, and that money goes to the dogs! So you might as well give up!", answer: "Harry: Give up? Give up! You're joking!")]
+    
     static let sharedInstance = CardManager()
     private override init() {}
+    
+    func setCardQuestion() -> String {
+        return cardArray[cardIndex].question
+    }
+    
+    func setCardAnswer() -> String {
+        let answer = cardArray[cardIndex].answer
+        cardIndex += 1
+        if cardIndex == cardArray.count {
+            cardIndex = 0
+        }
+        return answer
+    }
 }
