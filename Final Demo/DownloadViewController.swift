4e3ece061f759f792391eb435bd9099bf5a64a05
mlinglhl <minhung.ling@gmail.com>
2017-03-02 17:45:25 -0500

    Added closure to download manager

--- Final Demo/DownloadViewController.swift
+++ Final Demo/DownloadViewController.swift
@@ -9,31 +9,33 @@
 import UIKit
 
 class DownloadViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
+    
     @IBAction func createCards(_ sender: UIBarButtonItem) {
         let downloadManager = DownloadManager()
-        downloadManager.makeCardsWithUrl("https://sheetsu.com/apis/v1.0/be01e80a78d1")
+        downloadManager.makeCardsWithUrl("https://sheetsu.com/apis/v1.0/be01e80a78d1", completion: {
+            let _ = self.navigationController?.popViewController(animated: true)
+        })
     }
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
