9c393b20a4b735d7648425a6b49a61a1143b2295
Tristan Wolf <wolftristan@hotmail.com>
2017-03-08 22:13:58 -0500

    Flipped colour gradient on downloadViewController and homeViewController

--- Final Demo/DownloadViewController.swift
+++ Final Demo/DownloadViewController.swift
@@ -20,7 +20,7 @@ class DownloadViewController: UIViewController {
         super.viewDidLoad()
 
         let newLayer = CAGradientLayer()
-        newLayer.colors = [UIColor(red:1.00, green:0.00, blue:0.00, alpha:1.0).cgColor,UIColor(red:0.47, green:0.09, blue:0.12, alpha:1.0).cgColor, UIColor(red:0.29, green:0.13, blue:0.45, alpha:1.0).cgColor]
+        newLayer.colors = [UIColor(red:0.29, green:0.13, blue:0.45, alpha:1.0).cgColor,UIColor(red:0.47, green:0.09, blue:0.12, alpha:1.0).cgColor]
         
         newLayer.frame = view.frame
         
